37eb02fa6cb434f338883a8310b5077f9b79203f
Supremist <sergkarv@gmail.com>
2023-09-14 19:21:19 +0300

    Add highlight groups extractions

--- lua/custom/theme/init.lua
+++ lua/custom/theme/init.lua
@@ -61,6 +61,97 @@ function M.spec_from_base16(p)
   }
 end
 
+function M.parse_hl_line(hl_line)
+  local group_name, spec = string.match(hl_line, "([^ ]+)%s+xxx (.*)")
+  local cleared_prefix = "cleared"
+  if spec:sub(1, #cleared_prefix) == cleared_prefix then 
+    return group_name, {} 
+  end
+  local linked = string.match(spec, "links to ([^ ]+)")
+  if linked then return group_name, {link=linked} end
+  local group_value = {}
+   for k, v in string.gmatch(spec, "(%w+)=([^ ]+)") do
+     group_value[k] = v
+   end
+   return group_name, group_value
+end
+
+function M.extract_highlights()
+  local output = vim.split(vim.fn.execute "highlight", "\n")
+  local hl_lines = {}
+  for _, v in ipairs(output) do
+    if v ~= "" then
+  	  if v:sub(1, 1) == " " then
+  	    local part_of_old = v:match "%s+(.*)"
+  	    hl_lines[#hl_lines] = hl_lines[#hl_lines] .. part_of_old
+  	  else
+  	    table.insert(hl_lines, v)
+  	  end
+	end
+  end
+
+  local hl_groups = {}
+  for _, v in ipairs(hl_lines) do
+    local k, val = M.parse_hl_line(v)
+    hl_groups[k] = val
+  end
+  return hl_groups
+end
+
+function M.save_hl_digraph()
+  local file,err = io.open("highlights.txt",'w')
+  if not file then
+    print("error:", err) -- not so hard?
+    return
+  end
+  local groups = M.extract_highlights()
+  file:write("digraph Highlights {\n")
+  vim.print(groups)
+  for name, val in pairs(groups) do
+    if val.link then
+	  file:write(string.format("  %q -> %q;\n", val.link, name))
+	end
+  end
+  file:write("}\n")
+  file:close()
+end
+
+function M.save_hl_xml()
+ local file,err = io.open("highlights.xml",'w')
+  if not file then
+    print("error:", err) -- not so hard?
+    return
+  end
+  local groups = M.extract_highlights()
+  local tree = {}
+  for name, val in pairs(groups) do
+    if val.link then
+	  local children = tree[val.link] or {}
+	  table.insert(children, name)
+	  tree[val.link] = children
+	end
+  end
+  local visited = {}
+  local function print_node(node, level)
+    if visited[node] then return end
+	visited[node] = true
+	local name = string.gsub(node, "@", "AT")
+	for i = 0, level, 1 do file:write("  ") end
+    file:write(string.format("<%s>\n", name))
+	for _, v in ipairs(tree[node] or {}) do
+	  print_node(v, level+1)
+	end
+	for i = 0, level, 1 do file:write("  ") end
+	file:write(string.format("</%s>\n", name))
+  end
+  file:write("<highlights>\n")
+  for name, children in pairs(tree) do
+    print_node(name, 1)
+  end
+  file:write("</highlights>")
+  file:close()
+end
+
 
 function M.setup(opts)
   local hi = function (name, opts) 
@@ -272,22 +363,22 @@ function M.setup(opts)
   end
 
   -- folke/trouble.nvim
-  if H.has_integration('folke/trouble.nvim') then
-    hi('TroubleCount',           {fg=p.string,  bold=true, })
-    hi('TroubleFoldIcon',        {fg=p.fg.default,      })
-    hi('TroubleIndent',          {fg=p.bg.selection,      })
-    hi('TroubleLocation',        {fg=p.fg.calm,      })
-    hi('TroubleSignError',       {link='DiagnosticError'})
-    hi('TroubleSignHint',        {link='DiagnosticHint'})
-    hi('TroubleSignInformation', {link='DiagnosticInfo'})
-    hi('TroubleSignOther',       {link='DiagnosticInfo'})
-    hi('TroubleSignWarning',     {link='DiagnosticWarn'})
-    hi('TroubleText',            {fg=p.fg.default,      })
-    hi('TroubleTextError',       {link='TroubleText'})
-    hi('TroubleTextHint',        {link='TroubleText'})
-    hi('TroubleTextInformation', {link='TroubleText'})
-    hi('TroubleTextWarning',     {link='TroubleText'})
-  end
+  -- if H.has_integration('folke/trouble.nvim') then
+    -- hi('TroubleCount',           {fg=p.string,  bold=true, })
+    -- hi('TroubleFoldIcon',        {fg=p.fg.default,      })
+    -- hi('TroubleIndent',          {fg=p.bg.selection,      })
+    -- hi('TroubleLocation',        {fg=p.fg.calm,      })
+    -- hi('TroubleSignError',       {link='DiagnosticError'})
+    -- hi('TroubleSignHint',        {link='DiagnosticHint'})
+    -- hi('TroubleSignInformation', {link='DiagnosticInfo'})
+    -- hi('TroubleSignOther',       {link='DiagnosticInfo'})
+    -- hi('TroubleSignWarning',     {link='DiagnosticWarn'})
+    -- hi('TroubleText',            {fg=p.fg.default,      })
+    -- hi('TroubleTextError',       {link='TroubleText'})
+    -- hi('TroubleTextHint',        {link='TroubleText'})
+    -- hi('TroubleTextInformation', {link='TroubleText'})
+    -- hi('TroubleTextWarning',     {link='TroubleText'})
+  -- end
 
   -- folke/todo-comments.nvim
   -- Everything works correctly out of the box
