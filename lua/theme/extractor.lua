b54986574a3b4ef964d670ff14dd5a7985e69b6a
Supremist <sergkarv@gmail.com>
2023-10-04 19:59:08 +0300

    Extract theme as human readable lua table

--- lua/theme/extractor.lua
+++ lua/theme/extractor.lua
@@ -1,5 +1,13 @@
 local M = {}
 
+local color_params = {"fg", "bg", "sp", "ctermfg", "ctermbg"}
+
+local function add_attrs(attrs, attr_str)
+  for _, attr in ipairs(vim.split(attr_str, ",")) do
+    attrs[attr] = true
+  end
+end
+
 function M.parse_hl_line(hl_line)
   local group_name, spec = string.match(hl_line, "([^ ]+)%s+xxx (.*)")
   local cleared_prefix = "cleared"
@@ -10,10 +18,21 @@ function M.parse_hl_line(hl_line)
   if linked then return group_name, {link=linked} end
   local group_value = {}
    for k, v in string.gmatch(spec, "(%w+)=([^ ]+)") do
-     group_value[k] = v
+     if k:sub(1, 3) == "gui" then
+	   k = k:sub(4, -1)
+	 end
+	 if k == "" then
+	   add_attrs(group_value, v)
+	 elseif k == "cterm" then
+	   group_value["cterm"] = {}
+	   add_attrs(group_value["cterm"], v)
+	 else
+	   group_value[k] = v
+	 end
    end
    return group_name, group_value
 end
+
 
 function M.extract_highlights()
   local output = vim.split(vim.fn.execute "highlight", "\n")
@@ -37,6 +56,90 @@ function M.extract_highlights()
   return hl_groups
 end
 
+function M.parse_hl_tree(hl_groups, palette)
+  palette = palette or {}
+  local tree = {}
+  local function add_link(from, to)
+    if not tree[from] then tree[from] = {} end
+	table.insert(tree[from], to)
+  end
+  for name, val in pairs(hl_groups) do
+    local has_link = false
+    if val.link then
+	  add_link(val.link, name)
+	  has_link = true
+	else
+      for _, color_param in ipairs(color_params) do
+	    if val[color_param] then
+		  local color_val = val[color_param]
+		  local color_name = ""
+		  if palette[color_val] then
+		    color_name = palette[color_val]
+		  else
+		    color_name = color_val:sub(1,1) == "#" and "cl_" .. color_val:sub(2,-1) or color_val
+			palette[color_val] = color_name
+		  end
+		  if not has_link then
+	        add_link(color_name, name)
+		    has_link = true
+		  end
+	    end
+	  end
+	end
+  end
+  return tree
+end
+
+function M.save_theme(hl_groups, palette)
+  palette = palette or {}
+  local file,err = io.open("theme.lua",'w')
+  if not file then
+    print("error:", err)
+    return
+  end
+  local tree = M.parse_hl_tree(hl_groups, palette)
+  local stack = {}
+  local visited = {}
+  file:write("local p = { -- palette\n")
+  for color, name in pairs(palette) do
+    if name ~= color then
+      file:write(string.format('  %s = "%s",\n', name, color))
+      table.insert(stack, name)
+	end
+  end
+  file:write("} -- palette\nlocal g = { -- highlight groups\n")
+  local level_count = #stack
+  local level = 0
+  file:write(" -- Level", level, " contains ", level_count, " groups\n")
+  while(#stack > 0) do
+    local top = stack[1]
+	table.remove(stack, 1)
+	level_count = level_count-1
+	if not visited[top] then
+	  visited[top] = true
+	  local hl = hl_groups[top]
+	  if hl then
+	    file:write(string.format('["%s"] = %s,\n', top, vim.inspect(hl):gsub("\n", "")))
+	  end
+	  for _, child in ipairs(tree[top] or {}) do
+	    table.insert(stack, child)
+	  end
+	end
+	if level_count == 0 then
+	  file:write("\n")
+	  level = level+1
+	  level_count = #stack
+	  file:write(" -- Level", level, " contains ", level_count, " groups\n")
+	end
+  end
+  file:write("}")
+  file:close()
+end
+
+function M.test()
+  M.save_theme(M.extract_highlights())
+end
+
 function M.save_hl_xml()
  local file,err = io.open("highlights.xml",'w')
   if not file then
@@ -44,14 +147,9 @@ function M.save_hl_xml()
     return
   end
   local groups = M.extract_highlights()
-  local tree = {}
-  for name, val in pairs(groups) do
-    if val.link then
-	  local children = tree[val.link] or {}
-	  table.insert(children, name)
-	  tree[val.link] = children
-	end
-  end
+  local palette = {}
+  local tree = M.parse_hl_tree(groups, palette)
+  vim.print(palette)
   local visited = {}
   local order = {}
   
