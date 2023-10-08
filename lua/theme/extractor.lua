738c46bf1ba0839099d5ed05d416d2a95bdc086a
Supremist <sergkarv@gmail.com>
2023-10-08 15:48:59 +0300

    Update theme extractions

--- lua/theme/extractor.lua
+++ lua/theme/extractor.lua
@@ -1,12 +1,14 @@
 local M = {}
 
-local color_params = {"fg", "bg", "sp", "ctermfg", "ctermbg"}
+local color_params = {"fg", "bg", "sp"}
 
 local function add_attrs(attrs, attr_str)
   for _, attr in ipairs(vim.split(attr_str, ",")) do
     attrs[attr] = true
   end
 end
+
+local skip_cterm = true
 
 function M.parse_hl_line(hl_line)
   local group_name, spec = string.match(hl_line, "([^ ]+)%s+xxx (.*)")
@@ -19,16 +21,16 @@ function M.parse_hl_line(hl_line)
   local group_value = {}
    for k, v in string.gmatch(spec, "(%w+)=([^ ]+)") do
      if k:sub(1, 3) == "gui" then
-	   k = k:sub(4, -1)
-	 end
-	 if k == "" then
-	   add_attrs(group_value, v)
-	 elseif k == "cterm" then
-	   group_value["cterm"] = {}
-	   add_attrs(group_value["cterm"], v)
-	 else
-	   group_value[k] = v
-	 end
+       k = k:sub(4, -1)
+     end
+     if k == "" then
+       add_attrs(group_value, v)
+     elseif k == "cterm" and not skip_cterm then
+       group_value["cterm"] = {}
+       add_attrs(group_value["cterm"], v)
+     elseif k:sub(1, 5) ~= "cterm" or not skip_cterm then
+       group_value[k] = v
+     end
    end
    return group_name, group_value
 end
@@ -39,13 +41,13 @@ function M.extract_highlights()
   local hl_lines = {}
   for _, v in ipairs(output) do
     if v ~= "" then
-  	  if v:sub(1, 1) == " " then
-  	    local part_of_old = v:match "%s+(.*)"
-  	    hl_lines[#hl_lines] = hl_lines[#hl_lines] .. part_of_old
-  	  else
-  	    table.insert(hl_lines, v)
-  	  end
-	end
+        if v:sub(1, 1) == " " then
+          local part_of_old = v:match "%s+(.*)"
+          hl_lines[#hl_lines] = hl_lines[#hl_lines] .. part_of_old
+        else
+          table.insert(hl_lines, v)
+        end
+    end
   end
 
   local hl_groups = {}
@@ -56,42 +58,75 @@ function M.extract_highlights()
   return hl_groups
 end
 
-function M.parse_hl_tree(hl_groups)
-  local tree = {}
+function invert_table(t)
+  local res = {}
+  for k, v in pairs(t) do
+    res[v] = k
+  end
+  return res
+end
+
+function M.parse_highlights(hl_groups)
+  local hl = {}
+  hl.groups = hl_groups
+  hl.tree = {}
+  hl.palette = {}
+  
+  function add_link(from, to)
+    if not hl.tree[from] then hl.tree[from] = {} end
+    table.insert(hl.tree[from], to)
+  end
+  
   local palette = {}
-  local function add_link(from, to)
-    if not tree[from] then tree[from] = {} end
-	table.insert(tree[from], to)
-  end
   for name, val in pairs(hl_groups) do
-    local has_link = false
-    if val.link then
-	  add_link(val.link, name)
-	  has_link = true
-	else
+    if next(val) == nil then
+      add_link("cleared", name)
+    elseif val.link then
+      add_link(val.link, name)
+    else
+      local has_color = false
       for _, color_param in ipairs(color_params) do
-	    local color_val = val[color_param]
-	    if color_val then
-		  palette[color_val] = true
-		  if not has_link then
-	        add_link(color_val, name)
-		    has_link = true
-		  end
-	    end
-	  end
-	end
+        local color_val = val[color_param]
+        if color_val then
+          palette[color_val] = true
+          if not has_color then
+            add_link(color_val, name)
+            has_color = true
+          end
+        end
+      end
+      if not has_color then
+        add_link("discolored", name)
+      end
+    end
   end
-  palette = require("theme.colors").generate_names(palette)
-  tree.ROOT = {}
-  for color, name in pairs(palette) do
-    tree[name] = tree[color]
-	tree[color] = nil
-	table.insert(tree.ROOT, name)
+  hl.palette = invert_table(require("theme.colors").generate_names(palette))
+  
+  hl.tree.color_names = {}
+  for name, color in pairs(hl.palette) do
+    if name ~= color then
+      hl.tree[name] = hl.tree[color]
+      hl.tree[color] = nil
+    end
+    table.insert(hl.tree.color_names, name)
   end
-  for name, children in pairs(tree) do
+  for name, children in pairs(hl.tree) do
     table.sort(children)
   end
-  return tree, palette
+  hl.tree.categories = {}
+  for i, name in ipairs(hl.tree.color_names) do
+    hl.tree.categories[i] = name
+  end
+  table.insert(hl.tree.categories, "discolored")
+  table.insert(hl.tree.categories, "cleared")
+  hl.tree.root = {}
+  for _, category in ipairs(hl.tree.categories) do
+    for _, name in ipairs(hl.tree[category] or {}) do
+      table.insert(hl.tree.root, name)
+    end
+  end
+  table.sort(hl.tree.root)
+  return hl
 end
 
 function M.save_theme(hl_groups)
@@ -102,52 +137,67 @@ function M.save_theme(hl_groups)
     print("error:", err)
     return
   end
-  local tree, palette = M.parse_hl_tree(hl_groups)
-  local stack = {}
-  local visited = {}
-  local color_by_name = {}
-  for color, name in pairs(palette) do
-    color_by_name[name] = color
-  end
+  local hl = M.parse_highlights(hl_groups)
   file:write("local p = { -- palette\n")
-  for _, name in ipairs(tree.ROOT) do
-    local color = color_by_name[name]
+  for _, name in ipairs(hl.tree.color_names) do
+    local color = hl.palette[name]
     if name ~= color then
       file:write(string.format('  %s = "%s",\n', name, color))
-	end
-	table.insert(stack, name)
+    end
   end
-  file:write("} -- palette\nlocal highlights = {\n")
-  local level_count = #stack
-  local level = 0
-  file:write(" -- Level", level, " contains ", level_count, " groups\n")
-  while(#stack > 0) do
-    local top = stack[1]
-	table.remove(stack, 1)
-	level_count = level_count-1
-	if top:sub(1,2) == "--" then
-	  file:write(" ", top, "\n")
-	elseif not visited[top] then
-	  visited[top] = true
-	  local hl = hl_groups[top]
-	  if hl then
-	    file:write(string.format('["%s"] = %s,\n', top, vim.inspect(hl):gsub("\n", "")))
-	  end
-	  if tree[top] then
-	    table.insert(stack, "-- "..top)
-	    for _, child in ipairs(tree[top]) do
-	      table.insert(stack, child)
-	    end
-	  end
-	end
-	if level_count == 0 then
-	  file:write("\n")
-	  level = level+1
-	  level_count = #stack
-	  file:write(" -- Level", level, " contains ", level_count, " groups\n")
-	end
+  file:write("} -- palette\n\nlocal highlights = {\n")
+  
+  for _, category in ipairs(hl.tree.categories) do
+    local groups = hl.tree[category] or {}
+    if #groups > 0 then
+      file:write(" -- ", category, "\n")
+      for _, group in ipairs(groups) do
+        local val = hl.groups[group]
+        file:write(string.format('["%s"] = %s,\n', group, vim.inspect(val):gsub("\n", "")))
+      end
+    end
   end
-  file:write("}")
+  file:write("} -- highlights\n")
+  
+  file:write("\nlocal links = {")
+  local visited = {}
+  
+  function indent(level)
+    file:write('\n')
+    for i = 0, level, 1 do file:write("  ") end
+  end
+  
+  function print_link(node, level)
+    level = level or 0
+    if visited[node] then return end
+    visited[node] = true
+    local children = hl.tree[node] or {}
+    if #children == 0 then
+      if level == 0 then 
+        return
+      end
+      indent(level)
+      file:write(string.format('["%s"] = {},', node))
+    else
+      indent(level)
+      file:write(string.format('["%s"] = {', node))
+      for _, child in ipairs(children) do
+        print_link(child, level+1)
+      end
+      indent(level)
+      file:write("},")
+    end
+  end
+  
+  for _, highlight in ipairs(hl.tree.root) do
+    print_link(highlight)
+  end
+  
+  file:write("\n} -- links")
+  for name, _ in pairs(visited) do
+    hl_groups[name] = nil
+  end
+  vim.print(hl_groups)
   file:close()
 end
 
@@ -168,25 +218,25 @@ function M.save_hl_xml()
   
   local function visit_node(node)
     if visited[node] then return end
-	visited[node] = true
-	for _, v in ipairs(tree[node] or {}) do visit_node(v) end
-	table.insert(order, 1, node)
+    visited[node] = true
+    for _, v in ipairs(tree[node] or {}) do visit_node(v) end
+    table.insert(order, 1, node)
   end
   
   local function print_node(node, level)
     if visited[node] then return end
-	visited[node] = true
-	
-	local name = string.gsub(node, "@", "AT_")
-	for i = 0, level, 1 do file:write("  ") end
-	if tree[node] then
+    visited[node] = true
+    
+    local name = string.gsub(node, "@", "AT_")
+    for i = 0, level, 1 do file:write("  ") end
+    if tree[node] then
       file:write(string.format("<%s>\n", name))
-	  for _, v in ipairs(tree[node]) do print_node(v, level+1) end
-	  for i = 0, level, 1 do file:write("  ") end
-	  file:write(string.format("</%s>\n", name))
-	else
-	  file:write(string.format("<%s/>\n", name))
-	end
+      for _, v in ipairs(tree[node]) do print_node(v, level+1) end
+      for i = 0, level, 1 do file:write("  ") end
+      file:write(string.format("</%s>\n", name))
+    else
+      file:write(string.format("<%s/>\n", name))
+    end
   end
   
   for name, children in pairs(tree) do
@@ -196,13 +246,13 @@ function M.save_hl_xml()
   visited = {}
   file:write("<highlights>\n")
   for _, node in ipairs(order) do
-	print_node(node, 0)
+    print_node(node, 0)
   end
   file:write("\n")
   for name, val in pairs(groups) do
     if not val.link and not visited[name] then
-	  print_node(name, 0)
-	end
+      print_node(name, 0)
+    end
   end
 
   file:write("</highlights>")
