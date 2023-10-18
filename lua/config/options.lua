343a9ee694afb29682d9bb4d18150e5b99e19956
Supremist <sergkarv@gmail.com>
2023-10-18 13:35:42 +0300

    Hybrid line numbers

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -11,3 +11,15 @@ opt.tabstop = 4
 opt.softtabstop = 4
 opt.shiftwidth = 4
 opt.expandtab = true
+
+opt.incsearch = true
+opt.hlsearch = false
+
+opt.ignorecase = true
+opt.smartcase = true
+
+opt.updatetime = 2000
+
+opt.number = true
+opt.relativenumber = true
+require("config.line_number")
