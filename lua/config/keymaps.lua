c71af0c4b111836abc35a21c2f7295cc138977f3
Supremist <sergkarv@gmail.com>
2023-10-23 21:06:20 +0300

    Consistent mapping for delete word in insert mode

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,8 +2,14 @@ local map = function(mode, from, to)
   vim.api.nvim_set_keymap(mode, from, to, {noremap = true})
 end
 
+--move up/down one *wrapped* line
 map("n", "j", "gj")
 map("n", "k", "gk")
 map("v", "j", "gj")
 map("v", "k", "gk")
+
+--<C-BS> is <C-H> because of terminal app
+--make <C-BS> delete previous word, and <C-W> can be consistent with normal mode
+map("i", "<C-H>", "<C-w>")
+map("i", "<C-w>", "<ESC><C-w>")
 
