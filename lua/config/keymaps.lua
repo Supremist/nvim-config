0a8edced5e9564741eeeda2d8f5c726ccd816d7d
Supremist <sergkarv@gmail.com>
2023-11-13 22:52:51 +0200

    Improve keymaps api and defaults

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -6,29 +6,46 @@
 -- [3]: (string|fun()) rhs
 -- [4]: (string) description (optional)
 -- ft: (StringArray) filetype for buffer-local keymaps
--- ... and all other arguments options for vim.keymap.set (see :map-arguments)
+-- buffer: (integer|boolean) Creates buffer-local mapping, 0 or true for current buffer
+-- remap: (boolean) Make the mapping recursive. Inverse of noremap. Defaults to false
+-- wait: (boolean) Wait for input in case of mapping conflict. Inverse of nowait. Defaults to true
+-- expr: (boolean) Evaluate rhs. Same as :h expr. Dafaults to false
+-- replace_keycodes: (boolean) When "expr" is true, replace keycodes in the resulting string (see nvim_replace_termcodes()). Returning nil from the Lua "callback" is equivalent to returning an empty string.
+-- silent: (boolean) - Do not echo mapping. Defaults to true
+-- script, unique - same as original :map-arguments
 -- where StringArray is (string|string[]) but if string contains "," then it will be splitted
 
 local require = require("lazy.core.util").lazy_require
 local Util = require("util")
+-- Just more compact
+local n = "n"
+local i = "i"
+
 
 local M = {}
 M.plugins = {}
 
 M.global = {
-  {"n,v", "j", "gj", "move down one *wrapped* line"},
-  {"n,v", "k", "gk", "move up one *wrapped* line"},
+-- mode   lhs  rhs                          description
+  {"n,x", "k", "v:count == 0 ? 'gk' : 'k'", "move up one *wrapped* line",   expr = true},
+  {"n,x", "j", "v:count == 0 ? 'gj' : 'j'", "move down one *wrapped* line", expr = true},
 
-  {"i", "<C-H>", "<C-w>", "delete previous word"}, -- <C-BS> is <C-H> because of terminal app
-  {"i", "<C-w>", "<ESC><C-w>", "window menu from insert mode"},
+  {"n,x", "↑", "v:count == 0 ? 'gk' : 'k'", "move up one *wrapped* line",   expr = true},
+  {"n,x", "↓", "v:count == 0 ? 'gj' : 'j'", "move down one *wrapped* line", expr = true},
+
+  {i, "↑", "<C-o>gk", "move up one *wrapped* line"},
+  {i, "↓", "<C-o>gj", "move down one *wrapped* line"},
+
+  {i, "<C-H>", "<C-w>", "delete previous word"}, -- <C-BS> is <C-H> because of terminal app
+  {i, "<C-w>", "<ESC><C-w>", "window menu from insert mode"},
 }
 
 local cmd = require("neo-tree.command")
 M.plugins["neo-tree.nvim"] = {
-  {"n", "<L>fe", function() cmd.execute({toggle = true, dir = Util.get_root()}) end, "Explorer NeoTree (root dir)"},
-  {"n", "<L>fE", function() cmd.execute({toggle = true, dir = vim.loop.cwd()}) end, "Explorer NeoTree (cwd)"},
-  {"n", "<L>e", "<L>fe", "Explorer NeoTree (root dir)", remap = true },
-  {"n", "<L>E", "<L>fE", "Explorer NeoTree (cwd)", remap = true },
+  {n, "<L>fe", function() cmd.execute({toggle = true, dir = Util.get_root()}) end, "Explorer NeoTree (root dir)"},
+  {n, "<L>fE", function() cmd.execute({toggle = true, dir = vim.loop.cwd()}) end, "Explorer NeoTree (cwd)"},
+  {n, "<L>e", "<L>fe", "Explorer NeoTree (root dir)", remap = true },
+  {n, "<L>E", "<L>fE", "Explorer NeoTree (cwd)", remap = true },
 }
 
 return M
--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -1,4 +1,36 @@
-local function split(str, sep)
+local function get_options(keymap)
+  local rhs = keymap.rhs
+  local opts = {
+    noremap = keymap.noremap or not keymap.remap, -- Defaults to true
+    nowait = keymap.nowait or keymap.wait == false, -- Defaults to false
+    silent = keymap.silent ~= nil and keymap.silent or true, -- Defaults to true
+  }
+  for _, opt_name in ipairs({"desc", "replace_keycodes", "silent", "script", "unique", "expr"}) do
+    opts[opt_name] = keymap[opt_name]
+  end
+  if type(rhs) == "function" then
+    opts.callback = rhs
+    rhs = ""
+  end
+  return rhs, opts
+end
+
+local M = {}
+local ft_augroup = vim.api.nvim_create_augroup("FileTypeKeymapsGlobal", {clear = true})
+
+function M.expand(str)
+  if type(str) ~= "string" then
+    return str
+  end
+  local res = str:gsub("<[lL]>", "<Leader>")
+  res = res:gsub("<[lL][lL]>", "<LocalLeader>")
+  res = res:gsub("<[pP]>", "<Plug>")
+  res = res:gsub("↑", "<Up>")
+  res = res:gsub("↓", "<Down>")
+  return res
+end
+
+function M.split(str, sep)
   if str == nil or type(str) == "table" then
     return str
   end
@@ -12,52 +44,124 @@ local function split(str, sep)
   return res
 end
 
-local function expand_keys(str)
-  if type(str) ~= "string" then
-    return str
-  end
-  local res = str:gsub("<[lL]>", "<Leader>")
-  res = res:gsub("<[lL][lL]>", "<LocalLeader>")
-  return res
-end
-
-local function copy(t)
+function M.parse(keymaps)
   local res = {}
-  for k,v in pairs(t) do
-    res[k] = v
+  if #keymaps == 0 or keymaps[1][1] == nil then
+    return keymaps -- Already parsed
   end
-  return res
-end
-
-local M = {}
-
-function M.toLazyKeySpec(keymaps)
-  local res = {}
   for i,keymap in ipairs(keymaps) do
-    local spec = copy(keymap)
-    spec.mode = split(spec[1])
-    spec.ft = split(spec.ft)
-    spec[1] = expand_keys(spec[2])
-    spec[2] = expand_keys(spec[3])
-    spec.desc = spec[4]
-    spec[4] = nil
-    spec[3] = nil
+    local spec = vim.deepcopy(keymap)
+    if spec[1] ~= nil then
+      spec.mode = M.split(spec[1])
+      spec.ft = M.split(spec.ft)
+      spec.lhs = M.expand(spec[2])
+      spec.rhs = M.expand(spec[3])
+      spec.desc = spec[4]
+      spec.buffer = keymap.buffer == true and 0 or keymap.buffer
+      spec[1] = nil
+      spec[2] = nil
+      spec[3] = nil
+      spec[4] = nil
+    end
     res[i] = spec
   end
   return res
 end
 
-function M.set(keymaps)
-  for _,keymap in ipairs(keymaps) do
-    local opts = copy(keymap)
-    opts[1] = nil
-    opts[2] = nil
-    opts[3] = nil
-    local ft = split(keymaps.ft) -- TODO bind only for filetype buffer
-    opts.desc = opts[4]
-    opts[4] = nil
-    vim.keymap.set(split(keymap[1]), expand_keys(keymap[2]), expand_keys(keymap[3]), opts)
+function M.toLazyKeySpec(keymaps)
+  local res = {}
+  for i,keymap in ipairs(M.parse(keymaps)) do
+    local spec = vim.deepcopy(keymap)
+    spec[1] = spec.lhs
+    spec[2] = spec.rhs
+    spec.nowait = not spec.wait
+    spec.lhs = nil
+    spec.rhs = nil
+    spec.wait = nil
+    res[i] = spec
   end
+  return res
+end
+
+function M.cmd(command)
+  return "<CMD>"..command.."<CR>"
+end
+
+function M.termcodes(keys)
+  return vim.api.nvim_replace_termcodes(keys, true, true, true)
+end
+
+function M._set_keymap(keymap, buffer)
+  if buffer == nil then
+    buffer = keymap.buffer
+  end
+  local lhs = keymap.lhs
+  local rhs, opts = get_options(keymap)
+  if buffer then
+    for _, mode in ipairs(keymap.mode) do
+      vim.api.nvim_buf_set_keymap(buffer, mode, lhs, rhs, opts)
+    end
+  else
+    for _, mode in ipairs(keymap.mode) do
+      vim.api.nvim_set_keymap(mode, lhs, rhs, opts)
+    end
+  end
+end
+
+function M.set(keymaps)
+  for _, keymap in ipairs(M.parse(keymaps)) do
+    if keymap.ft then
+      -- This will create keymap only for new buffers
+      -- Use layer if you want to create/remove keymaps dynamicaly for already existing buffers
+      vim.api.nvim_create_autocmd("FileType", {
+        group = ft_augroup,
+        pattern = keymap.ft,
+        desc = "Set keymap depending on buffer filetype",
+        callback = function(ev)
+          M._set_keymap(keymap, ev.buf)
+        end,
+      })
+    else
+      M._set_keymap(keymap)
+    end
+  end
+end
+
+function M.del(keymaps)
+  for _, keymap in ipairs(M.parse(keymaps)) do
+    local modes = keymap.mode
+    local buffer = keymap.buffer == true and 0 or keymap.buffer
+    if buffer then
+      for _, mode in ipairs(modes) do
+        pcall(vim.api.nvim_buf_del_keymap, buffer, mode, keymap.lhs)
+      end
+    else
+      for _, mode in ipairs(modes) do
+        pcall(vim.api.nvim_del_keymap, mode, keymap.lhs)
+      end
+    end
+  end
+end
+
+function M.wrap(provider)
+  return setmetatable({}, {__index = function(t, method_name) -- return fake object
+    return function(...) -- which will have fake methods with arg forwarding
+      local args = {...}
+      return function(fallback) -- method returns a function which calls real object provider, and than method from real object
+        local res = provider()[method_name](table.unpack(args))
+        if res ~= nil and not res and fallback then
+          fallback()
+        end
+      end
+    end
+  end})
+end
+
+-- Usage:
+-- local fn = wrap_mod("module_name").method_name(arg1, arg2 ...)
+-- where fn is a function which will call the method_name() with args
+function M.wrap_mod(module)
+  return M.wrap(function() return require(module) end)
 end
 
 return M
