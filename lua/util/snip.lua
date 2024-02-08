266f7e5732d90c07b912b604a4921b808ef84830
Supremist <sergkarv@gmail.com>
2024-02-08 15:39:26 +0200

    WIP: ext_messages listener snippet

--- lua/util/snip.lua
+++ lua/util/snip.lua
@@ -1,5 +1,8 @@
 local keys = require "which-key.keys"
 
+local file = {}
+local M = {}
+
 local function get_upvalue(fn, name)
   local i = 1
   while true do
@@ -14,4 +17,23 @@ local function get_upvalue(fn, name)
   end
 end
 
-vim.print(require("config.keymaps").plugins["flash.nvim"]:reshape({"id", "rhs", "mode"}, {1}))
+function M.trace_messages()
+  local f,err = io.open("C:/Users/sergk/nvim_log.txt", "a")
+  if not f then
+    print("IO Error: ", err)
+    return
+  end
+  file = f
+  file:write("Tracing started\n")
+  local ns = vim.api.nvim_create_namespace('message_listener')
+  vim.ui_attach(ns, {ext_messages = true}, function(event, ...)
+    file:write(event, "\n")
+  end)
+end
+
+function M.stop_tracing()
+  file:close()
+end
+
+-- vim.print(require("config.keymaps").plugins["flash.nvim"]:reshape({"id", "rhs", "mode"}, {1}))
+return M
